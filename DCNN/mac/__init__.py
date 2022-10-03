5ae54869ad642d2f6fb19251987b21390284d6b4
Dan Jones <d.jones20@ic.ac.uk>
2022-10-03 11:18:09 -0400

    imports

--- DCNN/mac/__init__.py
+++ DCNN/mac/__init__.py
@@ -1,6 +1,6 @@
 from .src import util
 from .src import est 
-from .src import eval
-from .src import enc
+# from .src import eval
+# from .src import enc
 # from .src import nrtf
-from .src import sm
+# from .src import sm
