ea23ff33f634b071b695369762c49aa65d73bebc
Vikas Tokala <v.tokala@imperial.ac.uk>
2023-02-21 12:37:09 +0100

    test plot

--- test_metrics.py
+++ test_metrics.py
@@ -6,6 +6,7 @@ from train import train
 import torch
 from DCNN.trainer import DCNNLightningModule
 from DCNN.utils import evalFunction
+import plotly.graph_objects as go
 config = {
     "defaults": [
         "model",
@@ -41,7 +42,7 @@ config = {
         "stoi_weight": 10, #10
         "kurt_weight": 0,
         "avg_mode": "time",
-        "attention": False
+        "attention": True
     }
 }
 
@@ -53,7 +54,7 @@ config = compose("config")
 
 
 evalMet = evalFunction.EvalMetrics()
-MODEL_CHECKPOINT_PATH = "/Users/vtokala/Documents/Research/di_nn/DCNN/checkpoints/Complex_RNN_40E_iso.ckpt"
+MODEL_CHECKPOINT_PATH = "/Users/vtokala/Documents/Research/di_nn/DCNN/checkpoints/Complex-Attn-40E-iso-com.ckpt"
 # MODEL_CHECKPOINT_PATH = "/kaggle/input/lss-resources/code/se/demo/last.ckpt"
 model = DCNNLightningModule(config)
 model.eval()
@@ -68,8 +69,66 @@ NOISY_DATASET_PATH =  "/Users/vtokala/Documents/Research/Databases/Dataset_Binau
 CLEAN_DATASET_PATH = '/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/SSN/Clean_testset'
 
 
-noisy_snr_l, noisy_snr_r, enhanced_snr_r, enhanced_snr_l, masked_ild_error, masked_ipd_error, improved_mbstoi, improved_snr_l,improved_snr_r, improved_stoi_l,improved_stoi_r = evalMet(NOISY_DATASET_PATH,
-                                                                                                                                                                                        CLEAN_DATASET_PATH,
-                                                                                                                                                                                        model)
+# noisy_snr_l, noisy_snr_r, enhanced_snr_r, enhanced_snr_l, masked_ild_error, masked_ipd_error, improved_mbstoi, improved_snr_l,improved_snr_r, improved_stoi_l,improved_stoi_r = evalMet(NOISY_DATASET_PATH,
+                                                                                                                                                                                        # CLEAN_DATASET_PATH,
+                                                                                                                                                                                        # model)
 
 
+NOISY_DATASET_PATH =  "/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/SSN/Noisy_testset"
+CLEAN_DATASET_PATH = '/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/SSN/Clean_testset'
+
+noisy_snr_l_ssn, noisy_snr_r_ssn, enhanced_snr_r_ssn, enhanced_snr_l_ssn, masked_ild_error_ssn, masked_ipd_error_ssn, improved_mbstoi_ssn, improved_snr_l_ssn,improved_snr_r_ssn, improved_stoi_l_ssn,improved_stoi_r_snn = evalMet(NOISY_DATASET_PATH,
+                                                                                                                                                                                                                                    CLEAN_DATASET_PATH,model)
+
+NOISY_DATASET_PATH =  "/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/WGN/Noisy_testset"
+CLEAN_DATASET_PATH = '/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/WGN/Clean_testset'
+
+
+noisy_snr_l_wgn, noisy_snr_r_wgn, enhanced_snr_r_wgn, enhanced_snr_l_wgn, masked_ild_error_wgn, masked_ipd_error_wgn, improved_mbstoi_wgn, improved_snr_l_wgn,improved_snr_r_wgn, improved_stoi_l_wgn,improved_stoi_r_wgn = evalMet(NOISY_DATASET_PATH,
+                                                                                                                                                                                                                                     CLEAN_DATASET_PATH,
+                                                                                                                                                                                                                                          model)
+
+NOISY_DATASET_PATH =  "/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/Factory/Noisy_testset"
+CLEAN_DATASET_PATH = '/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/Factory/Clean_testset'
+
+
+
+noisy_snr_l_fac, noisy_snr_r_fac, enhanced_snr_r_fac, enhanced_snr_l_fac, masked_ild_error_fac, masked_ipd_error_fac, improved_mbstoi_fac, improved_snr_l_fac,improved_snr_r_fac, improved_stoi_l_fac,improved_stoi_r_fac = evalMet(NOISY_DATASET_PATH,
+                                                                                                                                                                                                                                     CLEAN_DATASET_PATH,
+                                                                                                                                                                                                                                      model)
+NOISY_DATASET_PATH =  "/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/Office/Noisy_testset"
+CLEAN_DATASET_PATH = '/Users/vtokala/Documents/Research/Databases/Dataset_Binaural_2S/Testset_Experiments/Isotropic_testset/Office/Clean_testset'
+noisy_snr_l_off, noisy_snr_r_off, enhanced_snr_r_off, enhanced_snr_l_off, masked_ild_error_off, masked_ipd_error_off, improved_mbstoi_off, improved_snr_l_off,improved_snr_r_off, improved_stoi_l_off,improved_stoi_r_off = evalMet(NOISY_DATASET_PATH,
+                                                                                                                                                                                                                                    CLEAN_DATASET_PATH,
+                                                                                                                                                                                                                                        model)
+ 
+# x = ['WGN', 'SSN', 'Factory', 'Office']
+
+fig = go.Figure()
+
+fig.add_trace(go.Box(
+    y=masked_ild_error_wgn,
+    # x=x,
+    name='wgn',
+    # marker_color='#3D9970'
+))
+fig.add_trace(go.Box(
+    y=masked_ild_error_ssn,
+    # x=x,
+    name='SSN',
+    # marker_color='#3D9970'
+))
+fig.add_trace(go.Box(
+    y=masked_ild_error_fac,
+    # x=x,
+    name='Factory',
+    # marker_color='#3D9970'
+))
+fig.add_trace(go.Box(
+    y=masked_ild_error_off,
+    # x=x,
+    name='Office',
+    # marker_color='#3D9970'
+))
+fig.update_layout(yaxis_range=[0, 3])
+fig.show()
